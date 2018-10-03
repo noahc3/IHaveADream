71775708a5dd6667f12b7a3f0ac055f1cfd8afcf
noahc3 <noahcuroe@gmail.com>
2018-10-03 09:23:15 -0500

    Add project files.

---
+++ IHaveADream/IHaveADream/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Plugin.SimpleAudioPlayer;
+
+namespace IHaveADream {
+    public partial class MainPage : ContentPage {
+
+        //initialize audio players for each speech
+        ISimpleAudioPlayer playerMLK = CrossSimpleAudioPlayer.CreateSimpleAudioPlayer();
+        ISimpleAudioPlayer playerMalcolmX = CrossSimpleAudioPlayer.CreateSimpleAudioPlayer();
+
+        public MainPage() {
+            InitializeComponent();
+
+            //on initialization, load the speech audio into the players
+            playerMLK.Load("mlk.mp3");
+            playerMalcolmX.Load("malcolmx.mp3");
+        }
+
+        //event handler for MLK button on click
+        //when the button is clicked:
+        private void btnMLK_Clicked(object sender, EventArgs e) {
+            if (playerMLK.IsPlaying) {//if the MLK speech is playing, pause it
+                playerMLK.Pause();
+            } else { //otherwise:
+                if (playerMalcolmX.IsPlaying) { //if the malcolmx speech is playing:
+                    playerMalcolmX.Pause();//new pause the malcolm x speech
+                }
+
+                playerMLK.Play(); //then play the MLK speech
+            }
+        }
+
+        //event handler for MalcolmX button on click
+        private void btnMalcolmX_Clicked(object sender, EventArgs e) {
+            //pause MalcolmX if playing, otherwise pause MLK if playing and play MalcolmX.
+            if (playerMalcolmX.IsPlaying) {
+                playerMalcolmX.Pause();
+            } else {
+                if (playerMLK.IsPlaying) {
+                    playerMLK.Pause();
+                }
+
+                playerMalcolmX.Play();
+            }
+        }
+    }
+}
